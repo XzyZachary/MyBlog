eeaf57538f576a68f87d5474ceb5b15a0e1b6b50
Jack Xu <xu.zhiyi@xsunt-sh.com>
2018-08-07 17:59:01 +0800

    add koa

--- koa/controller/blog.js
+++ koa/controller/blog.js
@@ -11,44 +11,80 @@ c.setOptions({
     senitize: true,
     smartLists: true,
     smartypants: false,
-    highlight: function (code){
+    highlight: function (code) {
         return require('highlight.js').highlightAuto(code).value;
     }
 })
 
 
 module.exports = {
-    async list (ctx, next){
-        let { keyword, pageindex = 1, pagesize = 10}  = ctx.request.query;
-        try{
-            let reg = new RegExp(keyword,'i')
+    //获取博客list
+    async list(ctx, next) {
+        let {
+            keyword,
+            pageindex = 1,
+            pagesize = 10
+        } = ctx.request.query;
+        try {
+            let reg = new RegExp(keyword, 'i')
             let data = await ctx.findPage(blogModel, {
-                $or: [
-                    {type: { $regex: reg}},
-                    {title: { $regex: reg}}
+                $or: [{
+                        type: {
+                            $regex: reg
+                        }
+                    },
+                    {
+                        title: {
+                            $regex: reg
+                        }
+                    }
                 ]
-            }, { createTime: 0, html: 0}, {limit: pagesize * 1, skip: (pageindex-1)*pagesize})
+            }, {
+                createTime: 0,
+                html: 0
+            }, {
+                limit: pagesize * 1,
+                skip: (pageindex - 1) * pagesize
+            })
             ctx.send(data)
-        }catch (e){
+        } catch (e) {
             console.log(e)
             ctx.sendError(e)
         }
     },
 
-    async add(ctx, next){
+    //新增博客
+    async add(ctx, next) {
         let paramsData = ctx.request.body;
         try {
-            let data = await ctx.findOne(blogModel,{title: paramsData.title})
-            if (data){
+            let data = await ctx.findOne(blogModel, {
+                title: paramsData.title
+            })
+            if (data) {
                 ctx.sendError('数据已存在，请重新添加')
-            }else{
+            } else {
                 paramsData.html = marked(paramsdata.html)
                 let data = await ctx.add(blogModel, paramsData)
                 ctx.send(paramsData)
             }
-        }catch (e){
+        } catch (e) {
             ctx.sendError(e)
+        }
+    },
+
+    //更新或删除博客（不做物理删除）
+    async update(ctx, next) {
+        let paramsData = ctx.request.body;
+        try {
+            paramsData.html = marked(paramsData.html);
+            let data = await ctx.update(blogModel, {
+                _id: paramsData._id
+            }, paramsData)
+            ctx.send()
+        } catch (e) {
+            if (e === '暂无数据') {
+                ctx.sendError(e)
+            }
         }
     }
 }
-
---
+++ koa/controller/user.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+import conf from '../../config'
+import userModel from '../../models/user'
+
+module.exports = {
+    async login (ctx, next){
+        
+    }
+}
