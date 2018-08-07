dab9d6d99c37f49c85de4db7f0d2b8fd6ad0e6f6
Zachary <zachary@ZacharydeMacBook-Pro.local>
2018-08-07 19:46:20 +0800

    add user login

--- koa/controller/user.js
+++ koa/controller/user.js
@@ -4,6 +4,11 @@ import userModel from '../../models/user'
 
 module.exports = {
     async login (ctx, next){
-        
+        let {useranme, pwd} = ctx.request.bodyl
+        try{
+
+        }catch(e){
+            
+        }
     }
 }
